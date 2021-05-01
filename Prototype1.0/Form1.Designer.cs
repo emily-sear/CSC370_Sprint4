b53e2e561735f132adcdfc3d8d554cda4cf15780
emily-sear <61364884+emily-sear@users.noreply.github.com>
2021-05-01 14:09:26 -0500

    Sprint 4- make the screen size a bit bigger so it is easier to see :)

--- Prototype1.0/Form1.Designer.cs
+++ Prototype1.0/Form1.Designer.cs
@@ -75,7 +75,7 @@ namespace Prototype1._0
             this.menuStrip1.MdiWindowListItem = this.viewToolStripMenuItem;
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(1180, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(2189, 38);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
@@ -190,35 +190,35 @@ namespace Prototype1._0
             // graphToolStripMenuItem
             // 
             this.graphToolStripMenuItem.Name = "graphToolStripMenuItem";
-            this.graphToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.graphToolStripMenuItem.Size = new System.Drawing.Size(203, 26);
             this.graphToolStripMenuItem.Text = "Graph";
             this.graphToolStripMenuItem.Click += new System.EventHandler(this.graphToolStripMenuItem_Click);
             // 
             // studentNamesToolStripMenuItem
             // 
             this.studentNamesToolStripMenuItem.Name = "studentNamesToolStripMenuItem";
-            this.studentNamesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.studentNamesToolStripMenuItem.Size = new System.Drawing.Size(203, 26);
             this.studentNamesToolStripMenuItem.Text = "Student Names";
             this.studentNamesToolStripMenuItem.Click += new System.EventHandler(this.studentNamesToolStripMenuItem_Click);
             // 
             // calculateGradesToolStripMenuItem
             // 
             this.calculateGradesToolStripMenuItem.Name = "calculateGradesToolStripMenuItem";
-            this.calculateGradesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.calculateGradesToolStripMenuItem.Size = new System.Drawing.Size(203, 26);
             this.calculateGradesToolStripMenuItem.Text = "Calculate Grades";
             this.calculateGradesToolStripMenuItem.Click += new System.EventHandler(this.calculateGradesToolStripMenuItem_Click);
             // 
             // clearAllDataToolStripMenuItem
             // 
             this.clearAllDataToolStripMenuItem.Name = "clearAllDataToolStripMenuItem";
-            this.clearAllDataToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.clearAllDataToolStripMenuItem.Size = new System.Drawing.Size(203, 26);
             this.clearAllDataToolStripMenuItem.Text = "Clear All Data";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox1.Image = global::Prototype1._0.Properties.Resources._118976197_183548856633170_5248159684347062085_o;
-            this.pictureBox1.Location = new System.Drawing.Point(712, 154);
+            this.pictureBox1.Location = new System.Drawing.Point(1215, 229);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(435, 457);
@@ -231,7 +231,7 @@ namespace Prototype1._0
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(709, 75);
+            this.label1.Location = new System.Drawing.Point(1212, 150);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(52, 17);
             this.label1.TabIndex = 3;
@@ -241,7 +241,7 @@ namespace Prototype1._0
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(709, 118);
+            this.label2.Location = new System.Drawing.Point(1212, 193);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 17);
             this.label2.TabIndex = 4;
@@ -250,7 +250,7 @@ namespace Prototype1._0
             // textBox1
             // 
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.Location = new System.Drawing.Point(768, 69);
+            this.textBox1.Location = new System.Drawing.Point(1271, 144);
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(55, 22);
@@ -260,7 +260,7 @@ namespace Prototype1._0
             // textBox2
             // 
             this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox2.Location = new System.Drawing.Point(768, 114);
+            this.textBox2.Location = new System.Drawing.Point(1271, 189);
             this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(55, 22);
@@ -270,7 +270,7 @@ namespace Prototype1._0
             // 
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(709, 636);
+            this.label3.Location = new System.Drawing.Point(1212, 711);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(61, 17);
             this.label3.TabIndex = 8;
@@ -280,7 +280,7 @@ namespace Prototype1._0
             // 
             this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(709, 662);
+            this.label5.Location = new System.Drawing.Point(1212, 737);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(129, 17);
             this.label5.TabIndex = 10;
@@ -290,7 +290,7 @@ namespace Prototype1._0
             // textBox3
             // 
             this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox3.Location = new System.Drawing.Point(845, 631);
+            this.textBox3.Location = new System.Drawing.Point(1348, 706);
             this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 22);
@@ -299,7 +299,7 @@ namespace Prototype1._0
             // textBox5
             // 
             this.textBox5.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox5.Location = new System.Drawing.Point(845, 657);
+            this.textBox5.Location = new System.Drawing.Point(1348, 732);
             this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 22);
@@ -318,17 +318,18 @@ namespace Prototype1._0
             // 
             this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 48);
+            this.dataGridView1.Location = new System.Drawing.Point(23, 47);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(677, 638);
+            this.dataGridView1.Size = new System.Drawing.Size(1157, 874);
             this.dataGridView1.TabIndex = 17;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(879, 85);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(1259, 67);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(194, 40);
             this.button1.TabIndex = 19;
@@ -340,7 +341,7 @@ namespace Prototype1._0
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1180, 716);
+            this.ClientSize = new System.Drawing.Size(1751, 932);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.progressBar1);
